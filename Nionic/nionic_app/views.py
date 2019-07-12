f056b9a18d2a10def575fd1af960dce6dd9bdeff
Alan <A01206107@itesm.mx>
2019-07-11 20:12:13 -0500

    Added animation to the services section

--- Nionic/nionic_app/views.py
+++ Nionic/nionic_app/views.py
@@ -4,6 +4,7 @@ from django.http import HttpResponse
 
 # Create your views here.
 
+
 def index(request):
     my_dict = {}
-    return render (request, 'nionic_app/index.html', context=my_dict)
+    return render(request, 'nionic_app/index.html', context=my_dict)
